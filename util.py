4291ecb0661f2186cab703c8a627068777d0d85d
bmcorser <bmcorser@gmail.com>
2016-05-16 08:53:07 +0100

    what seems to be the boggle

--- util.py
+++ util.py
@@ -66,14 +66,20 @@ def start_ngrok(for_port):
 def travis_encrypt(user_repo, value):
     pubkey_url = "https://api.travis-ci.org/repos/{0}/key".format(user_repo)
     pubkey_str = requests.get(pubkey_url).json()['key']
-    with tempfile.NamedTemporaryFile() as pubkey_file, tempfile.NamedTemporaryFile() as in_file, tempfile.NamedTemporaryFile() as out_file:
-        pubkey_file.write(pubkey_str.encode('ascii'))
-        in_file.write(value.encode('ascii'))
-        subprocess.check_call([
-            'openssl', 'rsautl', '-encrypt', '-pubin', '-inkey', pubkey_file.name, '-ssl', '-in', in_file.name, '-out', out_file.name
-        ])
-        out_file.seek(0)
-        return base64.b64encode(out_file.read())
+    pubkey_file = tempfile.NamedTemporaryFile(delete=False)
+    pubkey_file.write(pubkey_str.encode('ascii'))
+    pubkey_file.close()
+
+    in_file = tempfile.NamedTemporaryFile(delete=False)
+    in_file.write(value.encode('ascii'))
+    in_file.close()
+
+    out_file = tempfile.NamedTemporaryFile(delete=False)
+    subprocess.check_call([
+        'openssl', 'rsautl', '-encrypt', '-pubin', '-inkey', pubkey_file.name, '-ssl', '-in', in_file.name, '-out', out_file.name
+    ])
+    out_file.seek(0)
+    return base64.b64encode(out_file.read())
 
 
 def appveyor_encrypt(api_key, value):
